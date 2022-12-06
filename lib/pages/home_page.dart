d2ee003ee0e339c71963d81a49064c438bc57438
jazmin <correo@example>
2022-12-06 17:32:49 -0500

    Firestore

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,8 +2,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  //referencia de tareas
+
   CollectionReference tasksReference =
       FirebaseFirestore.instance.collection('tareasfirebase');
+
+  /*Stream<int> counter() async* {
+    for (int i = 0; i < 10; i++) {
+      yield i;
+      await Future.delayed(const Duration(seconds: 2));
+    }
+  }
+
+  Future<int> getNumber() async {
+    return 1000;
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +24,30 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Firebase Firestore"),
       ),
+      body: StreamBuilder(
+        stream: tasksReference.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snap) {
+          if (snap.hasData) {
+            QuerySnapshot collection = snap.data;
+            List<QueryDocumentSnapshot> docs = collection.docs;
+            List<Map<String, dynamic>> docsMap =
+                docs.map((e) => e.data() as Map<String, dynamic>).toList();
+            print(docsMap);
+            return ListView.builder(
+              itemCount: docsMap.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(docsMap[index]["title"]),
+                );
+              },
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+
       /* body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
